7b89607338220519514568a9360940f2d973f1b9
moonbrooke <smoeland@gmail.com>
2025-11-13 02:59:24 +0700

    switch to blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,64 +1,53 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"onsails/lspkind.nvim", -- Adds VS Code-like pictograms/icons to the completion menu
-		"saadparwaiz1/cmp_luasnip", -- Enables LuaSnip as a source for nvim-cmp autocompletion
-		{
-			"L3MON4D3/LuaSnip", -- Snippet engine for Neovim (write and expand code snippets)
-			version = "v2.*",
-			build = "make install_jsregexp",
+	'saghen/blink.cmp',
+	-- optional: provides snippets for the snippet source
+	dependencies = { 'rafamadriz/friendly-snippets' },
+
+	-- use a release tag to download pre-built binaries
+	version = '1.*',
+	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	-- build = 'cargo build --release',
+	-- If you use nix, you can build from source using latest nightly rust with:
+	-- build = 'nix run .#build-plugin',
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+		-- 'super-tab' for mappings similar to vscode (tab to accept)
+		-- 'enter' for enter to accept
+		-- 'none' for no mappings
+		--
+		-- All presets have the following mappings:
+		-- C-space: Open menu or open docs if already open
+		-- C-n/C-p or Up/Down: Select next/previous item
+		-- C-e: Hide menu
+		-- C-k: Toggle signature help (if signature.enabled = true)
+		--
+		-- See :h blink-cmp-config-keymap for defining your own keymap
+		keymap = { preset = 'default' },
+
+		appearance = {
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = 'mono'
 		},
-		"rafamadriz/friendly-snippets", -- Large collection of pre-made snippets for various languages
-		"hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for LSP-based autocompletion
-		"hrsh7th/cmp-buffer", -- nvim-cmp source for words from the current buffer
-		"hrsh7th/cmp-path", -- nvim-cmp source for filesystem paths
-		"hrsh7th/cmp-nvim-lsp-signature-help", -- function signatures
+
+		-- (Default) Only show the documentation popup when manually triggered
+		completion = { documentation = { auto_show = false } },
+
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
+		},
+
+		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" }
 	},
-	config = function()
-		local lspkind = require("lspkind")
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-
-		require("luasnip.loaders.from_vscode").lazy_load()
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol_text",
-					menu = {
-						codeium = "",
-						luasnip = "",
-						buffer = "",
-						path = "",
-						nvim_lsp = "🅻",
-					},
-				}),
-			},
-
-			mapping = cmp.mapping.preset.insert({
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-				["<Tab>"] = cmp.mapping.select_next_item(),
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-n>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
-
-			sources = {
-				{ name = "codeium" },
-				{ name = "luasnip" },
-				{ name = "nvim_lsp" },
-				{ name = "buffer" },
-				{ name = "path" },
-				{ name = "nvim_lsp_signature_help" },
-			},
-		})
-	end,
+	opts_extend = { "sources.default" }
 }
