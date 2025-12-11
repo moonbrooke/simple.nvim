303f6c68e9394fa6f951c14ea1e7d84bd62d4414
moonbrooke <smoeland@gmail.com>
2025-12-11 20:33:43 +0700

    presenting: config

--- lua/plugins/presenting.lua
+++ lua/plugins/presenting.lua
@@ -1,3 +1,15 @@
 vim.pack.add({ "https://github.com/sotte/presenting.nvim", })
 
-require('presenting').setup({})
+require('presenting').setup({
+    options = {
+        width = 60,
+    },
+    separator = {
+        markdown = "^==+",
+        org = "^*+ ",
+        adoc = "^==+ ",
+        asciidoctor = "^==+ ",
+    },
+    keep_separator = false,
+    parse_frontmatter = false,
+})
