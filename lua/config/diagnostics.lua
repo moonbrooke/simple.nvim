03098892acdbae4ba48aa764a884edf1329947a6
moonbrooke <smoeland@gmail.com>
2026-05-15 03:34:41 +0700

    lsp: diag edit

--- lua/config/diagnostics.lua
+++ lua/config/diagnostics.lua
@@ -1,7 +1,15 @@
 -- Diagnostic settings
 vim.diagnostic.config({
-    virtual_text = false,
+    virtual_text = true,
+    severity_sort = true,
     signs = true,
     underline = true,
+    float = {
+        style = "minimal",
+        border = "solid",
+        source = "if_many",
+        header = "",
+        prefix = "",
+    },
     update_in_insert = false,
 })
